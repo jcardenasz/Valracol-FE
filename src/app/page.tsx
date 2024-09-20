c42e485140e6fd3cbda3c6c5154c7d3804d3390c
jcardenasz <jcardenasz@unal.edu.co>
2024-09-20 16:18:17 -0500

    Added: icon placeholder, behavioural page _document

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import Image from "next/image";
 
 export default function Home() {
@@ -20,7 +21,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>Update your ass mami </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
