99bbcf985bc1e0997321a94dd08256ecbb229bc4
Zabalino <jcardenasz@unal.edu.co>
2024-11-03 21:26:30 -0500

    Feat: Card new things. NF 🟡. New searchBar (Only visual).🟡 Todo: Fix the cards sizes and elements.

--- src/app/pages/products/page.tsx
+++ src/app/pages/products/page.tsx
@@ -3,16 +3,30 @@ import CardComponent from "@/app/components/atoms/card/cardComponent";
 
 export default function products() {
     return (
-        <div className="min-h-screen p-8 pb-20 sm:p-20 bg-white text-black">
-            <h1>Products</h1>
-            <p>
-                We are a group of developers who are passionate about creating
-                applications that help people. We are always looking for new ways to
-                improve our skills and learn new technologies.
-            </p>
-                <div className="flex flex-wrap gap-4">
-                    <CardComponent />
-                </div>
+        <div className="min-h-screen pb-20 sm:p-8 bg-white text-black">
+            <div className="flex items-center align-middle mb-4">
+                <p className="mr-2 w-full text-lg">Aquí encontrarás los productos que buscas. ¿Deseas buscar más productos?</p>
+                <input
+                    type="text"
+                    placeholder="Buscar productos, marcas..."
+                    value={""}
+                    onChange={() => {
+                    }}
+                    className="p-2 border border-gray-300 rounded-md w-full hover:border-gray-500 focus:border-gray-500 focus:outline-none shadow-md"
+                />
+            </div>
+            <div>
+                <hr className="my-4 border-gray-300"/>
+            </div>
+            <div className="flex flex-wrap gap-4">
+                <CardComponent/>
+                <CardComponent/>
+                <CardComponent/>
+                <CardComponent/>
+                <CardComponent/>
+                <CardComponent/>
+                <CardComponent/>
+            </div>
         </div>
     )
 }
