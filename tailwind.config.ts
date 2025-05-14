dc4b6ec6a747871826c4bcb5856d4357426e80c3
jcardenasz <jcardenasz@unal.edu.co>
2025-05-14 14:33:21 -0500

    fix: "cargando productos" shadow color fix

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,8 +24,8 @@ const config: Config = {
             },
             keyframes: {
                 glow: {
-                    '0%, 100%': { color: '#000000', textShadow: '0 0 10px #ff0000, 0 0 20px #ff0000' },
-                    '50%': { color: '#ffffff', textShadow: '0 0 10px #ff0000, 0 0 20px #ff0000' },
+                    '0%, 100%': { color: '#000000', textShadow: '0 0 10px #ffffff, 0 0 10px #ffffff' },
+                    '50%': { color: '#ffffff', textShadow: '0 0 10px #ffffff, 0 0 20px #ffffff' },
                 },
             },
             animation: {
