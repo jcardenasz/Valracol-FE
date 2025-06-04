1be00df48a26c5d007af63abc2941df59696865e
jcardenasz <jcardenasz@unal.edu.co>
2025-06-03 21:10:15 -0500

    fix: Map using google maps embed api, removed ol

--- src/app/components/molecules/map/map.tsx
+++ src/app/components/molecules/map/map.tsx
@@ -1,61 +1,18 @@
-import React, { useEffect } from 'react';
-import 'ol/ol.css';
-import Map from 'ol/Map';
-import View from 'ol/View.js';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ.js';
-import { fromLonLat } from 'ol/proj';
-import { Point } from 'ol/geom';
-import VectorLayer from 'ol/layer/Vector';
-import VectorSource from 'ol/source/Vector';
-import { Feature } from 'ol';
-import { Icon, Style } from 'ol/style';
-import './map.css';
+import React from 'react';
 
 const MapComponent: React.FC = () => {
-    useEffect(() => {
-        const coordinates = fromLonLat([-72.39550987683344, 5.3361664710329]);
-
-        const point = new Point(coordinates);
-        const feature = new Feature(point);
-
-        const iconStyle = new Style({
-            image: new Icon({
-                src: '/images/location-pointer.png',
-                scale: 0.046,
-            }),
-        });
-
-        feature.setStyle(iconStyle);
-
-        const map = new Map({
-            target: 'map',
-            layers: [
-                new TileLayer({
-                    source: new XYZ({
-                        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    }),
-                }),
-                new VectorLayer({
-                    source: new VectorSource({
-                        features: [feature],
-                    }),
-                }),
-            ],
-            view: new View({
-                center: coordinates,
-                zoom: 16.5,
-            }),
-        });
-
-        return () => {
-            map.setTarget(undefined);
-        };
-    }, []);
-
+    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY;
     return (
         <div className="map-container">
-            <div id="map" className="map"></div>
+            <iframe
+                width="100%"
+                height="450"
+                style={{ border: '0', borderRadius: '8px' }}
+                loading="lazy"
+                allowFullScreen
+                referrerPolicy="no-referrer-when-downgrade"
+                src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=5.336028614724344,-72.3954750525791`}
+            />
         </div>
     );
 };
--- src/app/components/templates/Landing/Landing.tsx
+++ src/app/components/templates/Landing/Landing.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import dynamic from 'next/dynamic';
 
 const CarouselComponent = dynamic(() => import('@/app/components/atoms/carrousel/carouselComponent'), { ssr: false });
-
 const MapComponent = dynamic(() => import('@/app/components/molecules/map/map'), { ssr: false });
+
 const img = [
     "/images/valvulas.webp",
     "/images/valvulas.webp",
@@ -12,6 +12,7 @@ const img = [
     "/images/valvulas.webp",
     "/images/valvulas.webp"
 ];
+
 export function Landing() {
     return (
         <div>
@@ -21,33 +22,32 @@ export function Landing() {
                         <Transition to={"bottom"} className={"w-full"}>
                             <div className="text-center">
                                 <h1 className="p-3 text-2xl font-bold mb-4">¿Qué es Valracol?</h1>
-                            </div> {/* bg-white bg-opacity-10 rounded-xl */}
+                            </div>
                         </Transition>
                         <Transition to={"right"} className={"w-full"}>
-                            <section className=" mb-4">
+                            <section className="mb-4">
                                 <div className="px-3 sm:p-8 text-justify">
                                     <p className="text-lg text-justify">
-                                    Valracol es una empresa Colombiana dedicada a proporcionar soluciones
-                                    de alta calidad en el ámbito ferretero para la industria petrolera.
-                                    Brindamos servicio a todo tipo de generadores.
+                                        Valracol es una empresa Colombiana dedicada a proporcionar soluciones
+                                        de alta calidad en el ámbito ferretero para la industria petrolera.
+                                        Brindamos servicio a todo tipo de generadores.
                                     </p>
                                 </div>
                             </section>
                         </Transition>
                     </div>
-                    
+
                     <section className="mb-8">
                         <h2 className="text-2xl font-semibold mb-4">Galería de Imágenes</h2>
                         <div className="flex w-full h-full sm:w-3/4 lg:w-full mx-auto">
-                            <CarouselComponent images={img}/>
+                            <CarouselComponent images={img} />
                         </div>
                     </section>
 
-
-                    <section className="mb-8">
+                    <section className="mb-8 w-full">
                         <h2 className="text-2xl font-semibold mb-4">Estamos ubicados en:</h2>
                         <h2 className="text-xl font-regular mb-4">Carrera 19 #22-80 (Esquina) - Yopal, Casanare.</h2>
-                        <div className="w-full h-full sm:mx-auto">
+                        <div className="w-full h-full justify-center items-center">
                             <MapComponent />
                         </div>
                     </section>
